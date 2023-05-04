44c1a1c5ada1ca96aca8926b4be1bb36d0bf8bfb
Jeremy Weiland <jeremy6d@gmail.com>
2023-05-04 11:34:23 -0400

    add copyright on footer

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "site-content" div and all content after.
+ *
+ * @package Amalie
+ * @since Amalie 1.0
+ */
+?>
+<?php if( get_theme_mod( 'hide_copyright' ) == '') { ?>
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<p>&copy; 2021-2023 Other Selves Working Group and Jeremy Weiland</p>
+			<small>This Wordpress site layout is based on the Amalie Lite theme.</small>
+	    </div>
+		<!-- .site-info -->
+	</footer><!-- .site-footer -->
+<?php } // end if ?>
+	
+	</div><!-- .site-content -->
+
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
