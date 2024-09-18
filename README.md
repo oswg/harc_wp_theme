a5d3d7599b990664bce4c3e7420d42b394412fad
Jeremy Weiland <jeremy6d@gmail.com>
2024-09-18 11:08:12 -0400

    update copyright to remove oswg

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # harc_wp_theme
+
+This is the theme customized to HARC's needs
--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 <?php if( get_theme_mod( 'hide_copyright' ) == '') { ?>
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<p>&copy; 2021-<?php echo date('Y'); ?> Other Selves Working Group and Jeremy Weiland</p>
+			<p>&copy; 2021-<?php echo date('Y'); ?> High Altitude Receiving Center, Stephen Tyman, and Jeremy Weiland</p>
 			<small>This Wordpress site layout is based on the Amalie Lite theme.</small>
 	    </div>
 		<!-- .site-info -->
