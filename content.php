ebdc38b7878cee5e2af38a0ba54d2e0e412255dd
Jeremy Weiland <jeremy6d@gmail.com>
2024-12-08 14:49:28 -0500

    specify category for conditional rendering

--- content.php
+++ content.php
@@ -17,7 +17,7 @@
 
 	<header class="entry-header">
 		<div class="subtitle">
-			<?php if ( has_category( 'your-category-slug' ) ) { ?>
+			<?php if ( has_category( 'Training' ) ) { ?>
 				<span class="subtitle-line training-label">TRAINING</span>
 			<?php } ?>
 			<span class="subtitle-line"><?php echo get_post_meta(get_the_ID(), 'circle', true); ?></span>
